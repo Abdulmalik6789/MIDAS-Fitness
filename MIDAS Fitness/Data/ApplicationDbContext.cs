f25a0d19456e0791cd3f2cdb74cffb9d13cc6756
Abdulmalik6789 <Fadahunsimalik9@gmail.com>
2025-05-19 18:18:37 +0100

    initial commit

---
+++ MIDAS Fitness/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MIDAS_Fitness.Models;
+namespace MIDAS_Fitness.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        //public DbSet<Product> Products { get; set; }
+        //public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ MIDAS Fitness/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MIDAS_Fitness.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
